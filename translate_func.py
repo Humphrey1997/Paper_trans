86b8c191ecb974e5acc7c9c3edb96cbd2f505a4e
Humphrey1997 <hanjy1997@qq.com>
2023-06-20 20:43:36 +0900

    1.1 version

--- translate_func.py
+++ translate_func.py
@@ -1,7 +1,57 @@
 import openai
+import time
+import random
 
-def translate_text(text,  model='gpt-3.5-turbo'):
-    response = openai.ChatCompletion.create(
+# define a retry decorator
+def retry_with_exponential_backoff(
+    func,
+    initial_delay: float = 1,
+    exponential_base: float = 2,
+    jitter: bool = True,
+    max_retries: int = 5,
+    errors: tuple = (openai.error,),
+):
+    """Retry a function with exponential backoff."""
+
+    def wrapper(*args, **kwargs):
+        # Initialize variables
+        num_retries = 0
+        delay = initial_delay
+
+        # Loop until a successful response or max_retries is hit or an exception is raised
+        while True:
+            try:
+                return func(*args, **kwargs)
+
+            # Retry on specified errors
+            except errors as e:
+                # Increment retries
+                num_retries += 1
+
+                # Check if max retries has been reached
+                if num_retries > max_retries:
+                    raise Exception(
+                        f"Maximum number of retries ({max_retries}) exceeded."
+                    )
+
+                # Increment the delay
+                delay *= exponential_base * (1 + jitter * random.random())
+
+                # Sleep for the delay
+                time.sleep(delay)
+
+            # Raise exceptions for any errors not specified
+            except Exception as e:
+                raise e
+
+    return wrapper
+
+@retry_with_exponential_backoff
+def completions_with_backoff(**kwargs):
+    return openai.ChatCompletion.create(**kwargs)
+
+def translate_text(text):
+    response = completions_with_backoff(
         model="gpt-3.5-turbo",
         messages=[
         {"role": "system", "content": "Translate English latex text about plasma physics to Chinese latex text.Do not translate person's name.Be faithful or accurate in translation. Make the translation readable or intelligible. Be elegant or natural in translation.Do not add any additional text in the translation. Ensure that all percentage signs (%) are properly escaped"},
@@ -9,8 +59,7 @@ def translate_text(text,  model='gpt-3.5-turbo'):
         {"role": "system", "content": "- last closed surface:最外闭合磁面 "},
         {"role": "system", "content": "- kinetic:动理学 "},
 
-
-        {"role": "user", "content": f" The text to be translated is:'{text}'\n"}
+        {"role": "user", "content": f" The text to be translated is:'{text}'\n"},
     	]
     )
     
@@ -34,9 +83,8 @@ def translate_body(tex_file):
     st0 = r'\maketitle'
     st1 = r'\section'
     st2 = r'{References}'
-    sx1 = r'{figure}'
-    sx2 = r'{equation}'
-    sx3 = r'{table}'
+    sx1 = r'\begin'
+    sx2 = r'\end'
     
     print('Translate main body')
 
@@ -52,7 +100,7 @@ def translate_body(tex_file):
     for line_idx in range(start_idx,end_idx):
         line = tex_file[line_idx]
         print(line_idx-start_idx+1,'/',L)
-        if sx1 in line or sx2 in line or sx3 in line:
+        if sx1 in line or sx2 in line:
             flag = not flag
             continue
         if flag and len(line)>2:
